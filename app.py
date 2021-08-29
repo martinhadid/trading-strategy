01d13067617952bb3bd40bcbe83119feddebd79d
Martin Hadid <martin@safemode.co>
2021-08-29 11:19:02 +0300

    separate responsibilities between data extraction and trading strategy summarize steps on app step

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import pandas as pd
+import yfinance as yf
+
 from configuration import STOCK, PERIOD
 from plot.signals import plot_signals
-from trading.trading_strategy import request_ticker, ticker_close_price, \
-    price_moving_average_10_days, price_moving_average_20_days, \
-    generate_signals_from_crossovers, set_position
+from trading.trading_strategy import set_trading_strategy
+
+
+def request_ticker(ticker_name: str, period: str) -> pd.DataFrame:
+    ticker = yf.Ticker(ticker_name)
+    return ticker.history(period=period)
 
 
 def app():
     ticker = request_ticker(ticker_name=STOCK, period=PERIOD)
-    df_stock = ticker_close_price(df_ticker=ticker)
-    df_stock["SMA_10"] = price_moving_average_10_days(df_stock)
-    df_stock["SMA_20"] = price_moving_average_20_days(df_stock)
-    df_stock_signals = generate_signals_from_crossovers(df_signals=df_stock)
-    df_stock_position = set_position(df_signals=df_stock_signals)
+    df_stock_position = set_trading_strategy(ticker=ticker)
     plot_signals(df=df_stock_position)
 
 
