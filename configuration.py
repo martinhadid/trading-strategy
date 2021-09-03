7b28eaff11a3d1e1ae085d0d09ec03f1de083473
Martin Hadid <martin@safemode.co>
2021-09-03 21:39:26 +0100

    add google stock

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-STOCKS = ("AMZN", "AAPL", "BABA", "FB", "MSFT", "NVDA", "ROKU", "SQ", "TSLA", "V")
+STOCKS = ("AMZN", "AAPL", "BABA", "FB", "GOOGL", "MSFT", "NVDA", "ROKU", "SQ", "TSLA", "V")
 VALID_PERIODS = ("1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max")
 STOP_LOSS = 0.06
